328ba3923d83ae9d64810c9c2df8abfb759d4228
User Name <email@domain.co.za>
2023-05-19 16:45:36 +0200

    Added a filter function to the search the monster list

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters:[],
+      monsters: [],
+      searchField: ''
     };
+    
+    console.log('consturctor');
   }
 
 
   componentDidMount() {
-    
+    console.log('componentDidmount');
+
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) =>
@@ -31,11 +35,27 @@ class App extends Component {
   }
    
   render() {
+    console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+     });
+
     return (
       <div className="App">
-        
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            const searchField = event.target.value.toLowerCase();
+            this.setState(() => {
+              return { searchField};
+            });
+          }}>
+        </input>     
         {
-          this.state.monsters.map((monster) => {
+          filteredMonsters.map((monster) => {
               return(
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
